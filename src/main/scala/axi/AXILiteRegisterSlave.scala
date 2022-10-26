2bacaae4ade47d5142703892b3d8bdd0a63527f9
Joey Harrison <joeyh021@icloud.com>
2022-10-26 15:27:30 +0100

    tidy up and comment

--- src/main/scala/axi/AXILiteRegisterSlave.scala
+++ src/main/scala/axi/AXILiteRegisterSlave.scala
@@ -7,58 +7,68 @@ import chisel3.util.experimental.forceName
 //simple peripheral with 4 64-bit registers
 //could be parametrised over n registers
 //axi-lite data width has to be either 32 or 64 bits
+//byte strobing not implemented
 class AXILiteRegisterSlave extends Module {
   val addrWidth = 2
-  val dataWidth = 64;
+  val dataWidth = 32;
   val io        = IO(new AXILiteSlave(addrWidth, dataWidth))
 
   //register with a vector of 4 64 bit numbers
   val registers = RegInit(VecInit(Seq.fill(4)(0.U(addrWidth.W))))
 
-  //generate ready signal for write address channel
-  val writeAddrReady = RegInit(false.B)
-  // registers can be written to when both write address and data are valid, a
-  val canWrite = io.writeAddr.valid && io.writeData.valid && !writeAddrReady
-  writeAddrReady := canWrite
-  io.writeAddr.ready := writeAddrReady
+  //--- ready/valid handshake registers ---
+  val writeAddrReadyReg = RegInit(false.B)
+  val writeDataReadyReg = RegInit(false.B)
+  val readAddrReadyReg  = RegInit(false.B)
+  val readDataValidReg  = RegInit(false.B)
+  val writeRespValidReg = RegInit(false.B)
 
-  //register to store write address
-  val writeAddr = RegInit(0.U)
-  when(canWrite) { writeAddr := io.writeAddr.bits.addr }
+  //--- write signalling ---
 
-  io.writeData.ready := RegNext(canWrite)
+  //connect awready signal
+  io.writeAddr.ready := writeAddrReadyReg
 
-  //write to registers
-  val doWrite        = io.writeData.ready && io.writeData.valid && writeAddrReady && io.writeAddr.valid
-  val writeRegSelect = writeAddr
+  // registers can be written to when both write address and data are valid
+  // and whatever the hell this last condition means
+  val canWrite = io.writeAddr.valid && io.writeData.valid && !writeAddrReadyReg
+  writeAddrReadyReg := canWrite
 
-  //write strobing not implemented, write entire word
-  when(doWrite) { registers(writeRegSelect) := io.writeData.bits.data }
+  //when can write, then store the address that was written to us
+  val writeAddrReg = RegInit(0.U)
+  when(canWrite) { writeAddrReg := io.writeAddr.bits.addr }
 
-  //generate write response signals
-  io.writeResponse.bits := 0.U //never error
-  val writeResponseValid = RegInit(false.B)
-  writeResponseValid := doWrite && !writeResponseValid
-  io.writeResponse.valid := writeResponseValid
+  //handle wready, use same canWrite wire
+  writeDataReadyReg := canWrite
+  io.writeData.ready := writeDataReadyReg
 
-  //generate read address ready signal
-  val readAddrReady = RegInit(false.B)
-  val canRead       = !readAddrReady && io.readAddr.valid
-  readAddrReady := canRead
-  io.readAddr.ready := readAddrReady
+  //write to registers when both write address and data channels are ready and valid
+  val doWrite = writeDataReadyReg && io.writeData.valid && writeAddrReadyReg && io.writeAddr.valid
+  when(doWrite) { registers(writeAddrReg) := io.writeData.bits.data }
 
-  //read address latch
-  val readAddr = RegInit(0.U)
-  when(canRead) { readAddr := io.readAddr.bits.addr }
+  //handle write response
+  io.writeResponse.bits := 0.U                       //never error
+  writeRespValidReg := doWrite && !writeRespValidReg //same weird pattern -- eduardo plz help
+  io.writeResponse.valid := writeRespValidReg
 
-  //generate read data valid and response signals
-  val readValid = RegInit(false.B)
-  val doRead    = readAddrReady && io.readAddr.valid && !readValid
-  readValid := doRead
-  io.readData.valid := readValid
-  io.readData.bits.resp := 0.U
+  // --- read signalling ---
 
+  //handle rready
+  val canRead = !readAddrReadyReg && io.readAddr.valid
+  readAddrReadyReg := canRead
+  io.readAddr.ready := readAddrReadyReg
+
+  //latch the read address passed to us
+  val readAddrReg = RegInit(0.U)
+  when(canRead) { readAddrReg := io.readAddr.bits.addr }
+
+  // read data is valid when address is valid and we are ready
+  val doRead = readAddrReadyReg && io.readAddr.valid && !readDataValidReg
+  readDataValidReg := doRead
+  io.readData.valid := readDataValidReg
+
+  io.readData.bits.resp := 0.U //always respond with okay
+
+  ///register select
   val readRegSelect = io.readAddr.bits.addr
-  val output        = RegNext(registers(readRegSelect))
-  io.readData.bits.data := output
+  io.readData.bits.data := RegNext(registers(readRegSelect)) //cannot have combinatorial paths from in->out
 }
