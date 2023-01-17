cb8077749e1542ae7dc26834a446d4b4e5466921
Joey Harrison <joeyh021@icloud.com>
2023-01-17 13:56:51 +0000

    add instruction memory

--- src/main/scala/dev/joeyh/pio/memory/InstructionMem.scala
+++ src/main/scala/dev/joeyh/pio/memory/InstructionMem.scala
@@ -2,13 +2,23 @@ package dev.joeyh.pio.memory
 
 import chisel3._
 import chisel3.util._
+import dev.joeyh.pio.util._
 
 //32x16bit instruction memory
 
-class InstructionMemIO extends Bundle {}
+class InstructionMemIO extends ReadWrite(UInt(16.W)) {
+  val address = Input(UInt(5.W))
+}
 
 class InstructionMem extends Module {
   val io = IO(new InstructionMemIO)
 
   val mem = Mem(32, UInt(16.W))
+
+  io.read := mem(io.address)
+
+  when(io.write.enable) {
+    mem(io.address) := io.write.data
+  }
+
 }
