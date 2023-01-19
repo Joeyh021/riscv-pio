7a723062a020b241228d1db0de63e0be328f6637
Joey Harrison <joeyh021@icloud.com>
2023-01-19 13:52:57 +0000

    add CSR

--- src/main/scala/dev/joeyh/pio/memory/CSR.scala
+++ src/main/scala/dev/joeyh/pio/memory/CSR.scala
@@ -2,19 +2,35 @@ package dev.joeyh.pio.memory
 
 import chisel3._
 import chisel3.util._
+import dev.joeyh.pio.util._
 
 //the bank of control and status registers for the PIO block
 //Uses a chisel Mem for combinational read, synchronous write
+//write 16 byte words at a time (could be bad if want to write bigger chunks)
+//literally just a smaller instruction memory
+//parametrised so can easily add more fields
 
-// address map:
-// 0x00 - clock divider config | 16 int | 8 frac | 8 unused
-// 0x04 - autopush/pull thresh | 5 push | 3 unused | 5 pull | 3 unused
-// 0x06 - wrap target
-// 0x08 pin dirs
-// 0x09 pin enables
+//address map
+object CSRAddresses {
+  val clockDividerInteger    = 0x00 //entire 16 bits is clock integer clock divisor
+  val clockDividerFractional = 0x02 //8 lower bits is fraction (byte 0x03 unused)
+  val autopushPull           = 0x04 //0x04 lower 5 is push, 0x05 lower 5 is pull
+  val wrapTarget             = 0x06 //lower 5 bits is wrap target
+  val pinConfigs             = 0xA  //TODO
+}
 
-class CSRIO extends Bundle {}
+class CSRIO extends ReadWrite(UInt(16.W)) {
+  val address = Input(UInt(5.W))
+}
 
-class CSR extends Module {
+class CSR(registers: Int) extends Module {
   val io = IO(new CSRIO)
+
+  val mem = Mem(registers, UInt(16.W))
+
+  io.read := mem(io.address)
+
+  when(io.write.enable) {
+    mem(io.address) := io.write.data
+  }
 }
