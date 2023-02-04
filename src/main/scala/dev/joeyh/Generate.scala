923d4871a0d8df1ebf14780445303fe1b5c4f4d1
Joey Harrison <joeyh021@icloud.com>
2023-02-04 17:19:38 +0000

    rename main

--- src/main/scala/dev/joeyh/Generate.scala
+++ src/main/scala/dev/joeyh/Generate.scala
@@ -1,7 +1,7 @@
 package dev.joeyh
 import chisel3.stage.ChiselStage
 
-object Main extends App {
+object Generate extends App {
   val defaultArgs      = Array("--target-dir", "gen/")
   val neaterOutputArgs = Array("--emission-options=disableMemRandomization,disableRegisterRandomization")
 
