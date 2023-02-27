99b190a9de450cb5191f3b2540227377825148e1
Joey Harrison <joeyh021@icloud.com>
2023-02-27 15:15:02 +0000

    fix clockdiv for 0

--- src/main/resources/vsrc/ClockDivider.v
+++ src/main/resources/vsrc/ClockDivider.v
@@ -2,24 +2,27 @@
 module ClockDivider(
     input clock,
     input [15:0] divisor,
-    output reg outClk
+    output wire outClk
 );
 
     reg [15:0] counter;
     reg [15:0] divisor_reg;
+    reg counted_clock;
     wire reset = (divisor != divisor_reg);
+    assign outClk = divisor == 16'b0 ? clock : counted_clock;
+
 
     always @ (posedge clock) begin
         if (reset) begin
             counter <= 16'b0;
-            outClk <= 1'b1;
+            counted_clock <= 1'b1;
             divisor_reg <= divisor;
         end else if (counter == divisor_reg - 16'b1) begin
             counter <= 16'b0;
-            outClk <= 1'b1;
+            counted_clock <= 1'b1;
         end else begin
             counter <= counter + 16'b1;
-            outClk <= 1'b0;
+            counted_clock <= 1'b0;
         end
     end
 
